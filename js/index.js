68a86ddfd8ca12b296b4dc612d45802040cde76b
lxx <814878176@qq.com>
2016-12-09 13:19:23 +0800

    index.html 轮播图 让图片初始的透明度设为0 撤销该操作

--- js/index.js
+++ js/index.js
@@ -4,22 +4,24 @@
 
 // ----------------------------------- 导航栏 Begin
 $(function(){
-    var navs = $('.nav li a');   // 导航DOM元素
+    var navs = $('.nav li');   // 导航DOM元素
     var navsContent = $('.nav-content li');   // 切换导航内容标签
     //for (var i = 0; i < navs.length; i++) {
     //    var nav = navs[i];
     //    nav.index = i ;
     //    $(nav).mouseenter(function () {
+    //        $(this).children("ul").stop().slideDown(600);
     //        $(navsContent).siblings().hide();
     //        $(navsContent[this.index]).show();
     //    })
-    //};  // -----------------这里封装成函数--> getOnly()
-    navs.getOnly(navsContent);  // 调用方法
-    $('.dm-nav').mouseleave(function () {
-        $(navsContent).hide();  //鼠标移开, 隐藏导航内容
+    //};  // -----------------这里封装成函数--> getTab()
+    navs.getTab(navsContent);  // 调用方法
+    $(navsContent).mouseleave(function () {
+        $(navsContent).hide();  //鼠标移开, 隐藏导航内容   ---------- 这里有缺陷
     });
 });
 // 导航栏 End
+<<<<<<< HEAD
 // ------------------------------------- 轮播图 Begin
 $(function () {
     var lbts = $('.dm-lbt li');
@@ -55,4 +57,6 @@ $(function () {
     })
 })
 // 轮播图 End
+=======
+>>>>>>> parent of 97bfc4e... index.html 轮播图   添加自动轮播功能
 
